3f6547e3b2f2ba088e1aeb1a79a9558c50e4a170
chrisIyer <hellohotbox420@gmail.com>
2024-03-24 16:34:29 -0400

    edit works on price. Somehow i can't edit the custom field.

--- src/components/OG.js
+++ src/components/OG.js
@@ -4,20 +4,37 @@ import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-quartz.css';
 
-
+const CustomButtonComponent = (props) => {
+     return <button onClick={() => window.alert('clicked') }>Push Me!</button>;
+   };
 export default function OG() {
      const [rowData, setRowData] = useState([
-          {make: 'Toyota', model: 'Celica', year: 2018, price:65897},
-          {make: 'Honda', model: 'Accord', year: 2018, price:25897},
-          {make: 'Nissan', model: 'Maxima', year: 2018, price:75897},
-          {make: 'Honda', model: 'Accord', year: 2018, price:62897},
+          {make: 'Toyota', model: 'Celica', year: 2018, price:65897, electronic: true},
+          {make: 'Honda', model: 'Accord', year: 2018, price:25897, electronic: false},
+          {make: 'Nissan', model: 'Maxima', year: 2018, price:75897, electronic: true},
+          {make: 'Honda', model: 'Civic', year: 2018, price:62897, electronic: false},
      ])
      const [colDefs, setColDefs] = useState([
-          {field: 'make'},
-          {field:'model'},
-          {field: 'year'},
-          {field: 'price'}
+          {
+               headerName: 'Make & Model',
+               valueGetter: (p) => p.data.make + ' ' + p.data.model,
+               flex: 2, filter: true, floatingFilter:true, editable: true
+             },
+             {
+               field: 'price',
+               valueFormatter: (p) => '£' + Math.floor(p.value).toLocaleString(),
+               flex: 1, editable: true
+             },
+             { field: 'electronic', flex: 1 },
+             { field: 'button', cellRenderer: CustomButtonComponent, flex: 1 },
+
+          {field: 'make', editable: true,cellEditor: 'agSelectCellEditor',
+          cellEditorParams: {
+              values: ['Honda', 'Nissan', 'Toyota'],
+          },
+  }
      ])
+     
      return (
           // wrapping container with theme & size
           <div
